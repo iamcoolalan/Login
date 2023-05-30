8343dce74dd352c30582630a30c925897a9b0765
iamcoolalan <iamalan1996@gmail.com>
2023-05-30 19:53:26 +0800

    feat: add Cookie-based Authentication by express-session

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const exhbs = require('express-handlebars')
 const bodyParser = require('body-parser')
+const session = require('express-session')
 const login = require('./utils/login')
 
 const PORT = process.env.PORT || 3000
@@ -11,21 +12,44 @@ app.set('view engine', 'hbs')
 
 app.use(express.static('public'))
 app.use(bodyParser.urlencoded({ extended: true }))
+app.use(session({
+  secret: 'test secret',
+  name: 'user',
+  saveUninitialized: false,
+  resave: true,
+}))
 
-app.get('/', (req, res) => {
+function auth(req, res, next) {
+  if(req.session.user){
+    return res.render('welcome', { firstName: req.session.user })
+  }else{
+    next()
+  }
+}
+
+app.get('/', auth, (req, res) => {
   res.render('login', { errorMessage: null })
 })
 
 app.post('/login', (req, res) => {
-  const userData = req.body
-  const firstName = login.matchUser(userData)
+  const { email, password } = req.body
+  const firstName = login.matchUser(email, password)
 
   if (firstName) {
-    res.render('welcome', { firstName })
+    req.session.user = firstName
+    return res.render('welcome', { firstName })
   } else {
-    res.render('login', { errorMessage: 'Username or password wrong. Please try again!' })
+    return res.render('login', { errorMessage: 'Username or password wrong. Please try again!' })
   }
 
+})
+
+app.get('/logout', (req, res) => {
+  req.session.destroy(() => {
+    console.log('session destoryed')
+  })
+
+  res.redirect('/')
 })
 
 app.listen(PORT, () => {
--- utils/login.js
+++ utils/login.js
@@ -1,9 +1,13 @@
 const users = require('../data/users.json')
 
 const login = {
-  matchUser: function (user) {
+  matchUser: function (email, password) {
+    if(email.trim() === '' || password.trim() === ''){
+      return null
+    }
+    
     for (const existedUser of users) {
-      if (existedUser.email === user.email && existedUser.password === user.password) {
+      if (existedUser.email === email && existedUser.password === password) {
         return existedUser.firstName
       }
     }
